c2afdb4acb0916f55964538250183a64138d1d0d
Jaepyoung Kim <jaepyoung.kim@ff.com>
2018-12-31 00:28:11 -0800

    Update

--- passwdparsing.py
+++ passwdparsing.py
@@ -1,21 +1,30 @@
 import sys
 import unittest
 import json
+import argparse
 
-# Getting parameter for password file and group file 
-#passwdfile = sys.argv[1] 
-#groupfile = sys.argv[2] 
-# def parse_args(args):
 
+
+## Define exception for wrong file format 
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+
+class FileFormatNotCorrect(Error):
+   """Raised when the input value is too small"""
+   pass
+
+# Function of open file.    
 def openfiles(filepath):
     filepathfh=""
     try: 
         filepathfh = open(filepath,'r')
     except FileNotFoundError:
-        print(filepath+" doesn't exist")
+        print(filepath+"Not found")
+        raise
     return filepathfh
 
-    
+# Getting the list of keys of dictionary with given value    
 def getKeysByValue(dictOfElements, valueToFind):
     listOfKeys = list()
     listOfItems = dictOfElements.items()
@@ -24,6 +33,7 @@ def getKeysByValue(dictOfElements, valueToFind):
             listOfKeys.append(item[0])
     return  listOfKeys
 
+# Getting groupname with given guid. 
 def getgroupfromgui(guid,groupfile):
     groupfilefh = openfiles(groupfile)
     groupname=""
@@ -34,13 +44,9 @@ def getgroupfromgui(guid,groupfile):
                 groupname = linelist[0]
     return groupname
 
-
-def passwordmerge(passwdfile, groupfile,mode):
-    if mode!="unittest":
-        passwdfile = sys.argv[1] 
-        groupfile = sys.argv[2] 
-    
-    # Assiginging default values
+# Main function 
+def passwordmerge(passwdfile, groupfile):
+    # Assiginging default values if the values are not provided. 
     if passwdfile is None: 
         passwdfile="/etc/passwd"
     if groupfile is None: 
@@ -54,8 +60,7 @@ def passwordmerge(passwdfile, groupfile,mode):
         if not line.startswith("#"):
             linelist  = line.split(":")
             if len(linelist) > 5 or len(linelist) <2: 
-                print("This is not group file")
-                return
+                raise FileFormatNotCorrect("It is not group file")
             if linelist[3] is not None:
                 groups[linelist[0]]=linelist[3]
             else:
@@ -65,14 +70,12 @@ def passwordmerge(passwdfile, groupfile,mode):
         if not line.startswith("#"):
             linelist = line.split(":")
             if len(linelist) < 6: 
-                print("This is not group file")
-                return
+                raise FileFormatNotCorrect("It is not passwd file")
             grouplist=getKeysByValue(groups,linelist[0])
             users[linelist[0]]={}
             users[linelist[0]]["uid"] = linelist[2]
             users[linelist[0]]["full_name"] = linelist[4]
             grouplist.append(getgroupfromgui(linelist[3],groupfile))
-
             users[linelist[0]]["groups"]= grouplist
             grouplist=[]
     json_data = json.dumps(users)
@@ -80,20 +83,47 @@ def passwordmerge(passwdfile, groupfile,mode):
     return json_data
 
 def main():
-    passwdfile=""
-    groupfile=""
-    mode="prod"
-    passwordmerge(passwdfile, groupfile,mode)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--passwd',help='path of passwd file')
+    parser.add_argument('--group',help='path of group file')
+    args = parser.parse_args()
+    passwdfile=args.passwd
+    groupfile=args.group
+    passwordmerge(passwdfile, groupfile)
 
 class TestStringMethods(unittest.TestCase):
-    #def test_checking_filesformat(self):
-    #    self.assertRaises(FileNotFoundError, passwordmerge("test","test","unittest"))
-    #def test_print_fileformats(self):
-    #    self.assertTrue(passwordmerge("/etc/passwd","test","unittest"))
-    def test_print_fileformats2(self):
-        self.assertTrue(passwordmerge("/etc/passwd","/etc/group","unittest"))
-    def test_print_fileformats3(self):
-        self.assertTrue(passwordmerge("./testpasswd","./testgroup","unittest"))
+    def test_exceptiong_notfoundpasswordfile(self):
+        with self.assertRaises(FileNotFoundError):
+            passwordmerge("incorrect","./testgroup") 
+    def test_exceptiong_notfoundgroupfile(self):
+        with self.assertRaises(FileNotFoundError):
+            passwordmerge("/etc/passwd","incorretgroup")  
+    def test_exceptiong_notwrongpasswdformatfile(self):
+        with self.assertRaises(FileFormatNotCorrect):
+            passwordmerge("/etc/hosts","/etc/group") 
+    def test_exceptiong_notwronggroupfile(self):
+        with self.assertRaises(FileFormatNotCorrect):
+            passwordmerge("/etc/passwd","/etc/hosts")  
+    def test_filesnotprovided(self):
+        self.assertEqual(passwordmerge(None,None),passwordmerge("/etc/passwd","/etc/group"))  
+    def test_paswdfilenotproivide(self):
+        self.assertEqual(passwordmerge("/etc/passwd",None),passwordmerge("/etc/passwd","/etc/group"))  
+    def test_groupfilenotprovided(self):
+        self.assertEqual(passwordmerge(None,"/etc/group"),passwordmerge("/etc/passwd","/etc/group"))      
+    def test_testgroup_testpasswd_fileschecking(self):
+        a=json.loads("""
+        {
+            "root": {"uid": "0", "full_name": "root", "groups": ["root"]},
+            "nobody": {"uid": "65534", "full_name": "nobody", "groups": ["nogroup"]}, 
+            "systemd-bus-proxy": {"uid": "103", "full_name": "systemd Bus Proxy,,,", "groups": ["systemd-bus-proxy"]}, 
+            "ntp": {"uid": "104", "full_name": "", "groups": ["ntp"]}, 
+            "statd": {"uid": "105", "full_name": "", "groups": ["nogroup"]}, 
+            "sshd": {"uid": "106", "full_name": "", "groups": ["nogroup"]}, 
+            "admin": {"uid": "1000", "full_name": "Debian", "groups": ["adm", "dialout", "cdrom", "floppy", "audio", "dip", "video", "plugdev", "admin"]}
+        }
+        """)
+        self.assertEqual(a,json.loads(passwordmerge("./testpasswd","./testgroup")))
+    
 
 
 if __name__ == '__main__':
